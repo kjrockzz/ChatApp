2790aed35a70dca02d737797059099e8a310e76d
kjrockzz <122872642+kjrockzz@users.noreply.github.com>
2024-05-11 13:49:10 +0530

    Update Constants.dart
    api key chng

--- lib/shared/Constants.dart
+++ lib/shared/Constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Constants {
   static String appId = "1:482510413269:web:a74d263c07b97157e9f15c";
-  static String apiKey = "AIzaSyAxIM9g9poHSbJAydY1HniR6tl3sW_VO2M";
+  static String apiKey = "api key";
   static String messagingSenderId = "482510413269";
   static String projectId = "chatappflutetr";
   final primaryColor = const Color(0xFFee7b64);
